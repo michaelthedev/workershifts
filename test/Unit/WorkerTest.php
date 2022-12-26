58cb6fded629d140c99fd62d60a11bf077effdb1
Michael A <39175160+michaelthedev@users.noreply.github.com>
2022-12-26 10:33:07 +0100

    Initial commit.

--- test/Unit/WorkerTest.php
+++ test/Unit/WorkerTest.php
@@ -12,7 +12,7 @@ class WorkerTest extends TestCase {
 	}
 
 
-	/*public function testGetWorkShifts() {
+	public function testGetWorkShifts() {
 		$worker = new Worker($this->worker_id);
 
 		$this->assertEmpty($worker->getWorkShifts());
@@ -23,5 +23,5 @@ class WorkerTest extends TestCase {
 		$expected = [
 			["shift_id"=>"63a753d20e311","worker_id"=>"WRK202212728","duration"=>"8","start_time"=>"1671910320","end_time"=>"1671939120","status"=>"present","date"=>"1671910354"]];
 		$this->assertSame($expected, $worker->getWorkShifts());
-	}*/
+	}
 }
